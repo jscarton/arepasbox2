54a051690d05b40777f246433c6a2deaf6d1b69f
jscarton <jscarton@gmail.com>
2016-04-06 10:01:48 -0500

    installing mongodb

--- vagrant_config/vagrant_provision_install.sh
+++ vagrant_config/vagrant_provision_install.sh
@@ -123,3 +123,13 @@ echo "INSTALLING SENSE"
 echo "---------------------------------------------------------------"
 sudo /opt/kibana-4.4.2-linux-x64/bin/kibana plugin --install elastic/sense
 
+echo "---------------------------------------------------------------"
+echo "INSTALLING MONGODB"
+echo "---------------------------------------------------------------"
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+echo "---------------------------------------------------------------"
+echo " AREPASBOX V2 PROVISIONING HAS BEEN COMPLETED"
+echo "---------------------------------------------------------------"
